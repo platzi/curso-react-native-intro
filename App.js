7e49a821052eb948f3e5cc1b33ab1c57d11dca6f
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-14 19:51:25 +0200

    Instalando y configurando React Navigation

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Curso React Native</Text>
-      <Saludar firstname="Agustin" lastname={28} />
-      <Saludar firstname="Carlos" lastname="Navarro" />
-      {/* <Saludar firstname="Andres" lastname="Navarro" /> */}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Curso React Native</Text>
+        <Saludar firstname="Agustin" lastname="Navarro Galdon" />
+      </View>
+    </NavigationContainer>
   );
 }
 
